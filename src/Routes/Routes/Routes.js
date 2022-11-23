5ad1d2ea994434736f194aa7011270ac3fa7f3ee
Md. Tarek Rahman <tarekrahman237@gmail.com>
2022-11-24 00:16:35 +0600

    added Navbar

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layouts/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Signup from "../../Pages/Signup/Signup";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
+
+export default router;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,30 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#00234D",
+
+          secondary: "#463AA1",
+
+          accent: "#1f2937",
+
+          neutral: "#021431",
+
+          "base-100": "#FFFFFF",
+
+          info: "#93E6FB",
+
+          success: "#80CED1",
+
+          warning: "#EFD8BD",
+
+          error: "#E58B8B",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
